e34c6db60cb905d0ef05ff6080bb423840aefb3c
Chettha.pj <chettha.pj@gmail.com>
2025-12-17 14:11:51 +0700

    create nevbar

---
+++ src/views/Layout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import { Navbar } from "../components/Navbar";
+
+
+
+export function Layout(){
+    return(
+        <div>
+            <Navbar />
+            <section>
+                <Outlet />
+            </section>
+        </div>
+    );
+}
--- src/views/Shop.jsx
+++ src/views/Shop.jsx
@@ -1,11 +1,11 @@
-export default function Home() {
-  return (
+export default function Shop (){
+return (
     <div className="flex flex-col items-center justify-center text-center pt-15">
-      <h1 className="font-bold leading-tight text-[48px] text-amber-600">
+      <h1 className="font-bold leading-tight text-[48px] text-red-500">
         เบื่อคุณส้มมมมมมมมมมมมมมมมมม
       </h1>
       <h2 className="font-bold leading-tight text-[48px]">
-          เมือ่ยยยยยยยยยยยยยยยยยยยยยยยย
+          555555555555555555555555555555555555555555555555555555555
       </h2>
       </div>
   );
