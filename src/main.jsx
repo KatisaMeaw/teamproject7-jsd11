b422c054b41f6ac1efc9616a1cba7c1e036d6c16
chalothorns <chalot.loy@gmail.com>
2025-12-23 17:38:32 +0700

    finished Register

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { Layout } from "./views/Layout.jsx";
 import Home from "./views/Home.jsx"
 import Shop from "./views/Shop.jsx"
 import { createRoot } from "react-dom/client";
+import Register from "./views/Register.jsx"
 
 const router = createBrowserRouter([
   {
@@ -18,6 +19,7 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element:<Home /> },
       { path: "/shop", element:<Shop /> },
+      { path: "/register", element:<Register /> },
       // { path: "/about", element:<About /> },
       // { path: "/contact", element:<Contact /> },
     ],
