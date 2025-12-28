958fa7a94bb102f981b871b72d5d4abae3176f33
Chettha.pj <chettha.pj@gmail.com>
2025-12-28 15:32:36 +0700

    version1

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,11 @@ import { Layout } from "./views/Layout.jsx";
 import Home from "./views/Home.jsx"
 import Shop from "./views/Shop.jsx"
 import { createRoot } from "react-dom/client";
-<<<<<<< HEAD
 import Register from "./views/Register.jsx"
-=======
 import ProductDetail from "./views/ProductDetail.jsx";
 import Cart from "./views/Cart.jsx";
 import Checkout from "./views/Checkout.jsx";
 import UserProfile from "./components/UserProfile.jsx";
->>>>>>> f0b6a6daa7e821200ee53444b2f3c54913ff9152
 
 const router = createBrowserRouter([
   {
@@ -25,16 +22,12 @@ const router = createBrowserRouter([
     ),
     children: [
       { path: "/", element:<Home /> },
-<<<<<<< HEAD
       { path: "/shop", element:<Shop /> },
       { path: "/register", element:<Register /> },
-=======
-      { path: "/shop", element:<Shop />},
       { path: "shop/:id", element:<ProductDetail />},
       { path: "/cart", element: <Cart />},
       { path: "/checkout", element: <Checkout />},
       { path: "/userprofile", element:<UserProfile />}
->>>>>>> f0b6a6daa7e821200ee53444b2f3c54913ff9152
       // { path: "/about", element:<About /> },
       // { path: "/contact", element:<Contact /> },
     ],
